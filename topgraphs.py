77e8bf7818d63eff09a313c65b839f198d6d7251
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-05-14 12:17:02 +0300

    title typo / most cases

--- topgraphs.py
+++ topgraphs.py
@@ -10,7 +10,7 @@ def show_most_cases(chart, df):
         param: chart name, dataframe
     '''
     stats= chart.split(sep='_')[-1]
-    graph = Graph('COVID-19 ' + stats + ' rate per country', 'countries', stats)
+    graph = Graph('COVID-19 ' + stats[:-1] + ' rate per country', 'countries', stats)
     top20 = df.groupby('location')[[chart]].sum().sort_values([chart])[-20:-1].reset_index()
     top20.plot(kind='barh', color='#336600', width=0.85, y=chart, x='location', ax=graph.ax)
     graph.set_info()
