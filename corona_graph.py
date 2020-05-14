b7c9e430922e3fd97cc481cfde2c3006ad2e0b7c
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-05-14 16:06:46 +0300

    refactoring

--- corona_graph.py
+++ corona_graph.py
@@ -4,10 +4,7 @@ import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 import numpy as np
 import pandas as pd
-from option_functions import choose_chart, choose_country, choose_time_period
-from topgraphs import show_most_cases
-from datetime import datetime, timedelta
-import matplotlib.dates as mdates
+from graph_functions import show_most_cases, compare_countries
 from classes import Graph
 register_matplotlib_converters()
 
@@ -17,47 +14,23 @@ results = dw.query(
     'SELECT * FROM full_data')
 df = results.dataframe
 
-stats = {'1':"total_cases", '2':"total_deaths", '3':"new_cases", '4':"new_deaths"}
-
 youngest = max(df['date'])
 oldest = min(df['date'])
 
-#Set style for graph
+# Print timestamp
+print(f"Updated: {youngest}")
+
+# Set style for all graphs
 plt.style.use('ggplot')
 
-#Additional graph
-show_most_cases(stats['3'], df)
-show_most_cases(stats['4'], df)
-
-# Choose time prediod
-stardate = choose_time_period(youngest)
+# Showing top 20 countries graphs
+show_most_cases('3', df)
+show_most_cases('4', df)
 
 # Sort country names
 df.drop(df.loc[df['location'] == "Cote d'Ivoire"].index, inplace=True)
 countries = np.sort(df.location.unique())
 df['location'] = df['location'].str.lower()
 
-# Choose stats
-chart = stats[choose_chart()]
-ylabel = chart.split(sep='_')[-1]
-
-# Creating new graph
-new_graph = Graph(chart.replace('_', ' ').title(), ylabel, 'date')
-new_graph.set_info()
-
-# Adding countries to graph
-print("\nOptions:\n\n".upper(), np.array2string(countries, max_line_width=150, separator=', ').replace("'", ''))
-countries = np.char.lower(countries.astype(str))
-while True:
-    new_country = choose_country(countries)
-    if not new_country:
-        break
-    new_df = df.loc[(df['location'] == new_country) & (df['date'] > stardate)].reset_index()
-    if chart == 'new_cases' or chart == 'new_deaths':
-        new_graph.ax.bar(new_df['date'], new_df[chart], alpha=0.5, label=new_country.title())
-    else:
-        new_graph.ax.plot(new_df['date'], new_df[chart], marker='.', label=new_country.title(), linewidth=2, markersize=12)
-
-# Adjust and show graph
-new_graph.ajust_graph(youngest, stardate)
-plt.show()
+# Compare countries graph
+compare_countries(df, countries, youngest)
---
+++ graph_functions.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from classes import Graph
+from option_functions import choose_chart, choose_country, choose_time_period
+import colorsys
+
+stats = {'1':"total_cases", '2':"total_deaths", '3':"new_cases", '4':"new_deaths"}
+
+# Generates list of colors
+def get_N_HexCol(N=20):
+    HSV_tuples = [(x * 1.0 / N, 0.5, 0.5) for x in range(N)]
+    hex_out = []
+    for rgb in HSV_tuples:
+        rgb = map(lambda x: int(x * 255), colorsys.hsv_to_rgb(*rgb))
+        hex_out.append('#%02x%02x%02x' % tuple(rgb))
+    return hex_out
+
+def show_most_cases(chart_num, df):
+    '''
+    Shows 20 countries statistic of most cases/deaths
+
+        param: chart name, dataframe
+    '''
+    chart = stats[chart_num]
+    stats_name = chart.split(sep='_')[-1]
+    graph = Graph('COVID-19 ' + stats_name[:-1] + ' rate per country', 'countries', stats_name)
+    top20 = df.groupby('location')[[chart]].sum().sort_values([chart])[-21:-1].reset_index()
+    top20.plot(kind='barh', color=get_N_HexCol(), width=0.85, y=chart, x='location', ax=graph.ax)
+    graph.set_info()
+    for i, country in enumerate(top20[chart]):
+        graph.ax.text(country, i, " "+str(country), va='center')
+    graph.ax.legend().set_visible(False)
+    plt.show()
+
+def compare_countries(df, countries, youngest):
+    '''
+    Allows user to choose time period of the graph 1 ,3 ,5 months.
+    User can choose countries to graph from 210 countries.
+    User can also choose statistics type from 1=total_cases, 2=total_deaths, 3=new_cases, 4=new_deaths.
+    Depending statistics type grap is bar graph (3,4) or line graph (1,2)
+
+        param: dataframe, countries array, youngest date
+    '''
+    stardate = choose_time_period(youngest)
+    chart = stats[choose_chart()]
+    ylabel = chart.split(sep='_')[-1]
+    new_graph = Graph(chart.replace('_', ' ').title(), ylabel, 'date')
+
+    new_graph.set_info()
+    print("\nOptions:\n\n".upper(), np.array2string(countries, max_line_width=150, separator=', ').replace("'", ''))
+    countries = np.char.lower(countries.astype(str))
+    while True:
+        new_country = choose_country(countries)
+        if not new_country:
+            break
+        new_df = df.loc[(df['location'] == new_country) & (df['date'] > stardate)].reset_index()
+        if chart == 'new_cases' or chart == 'new_deaths':
+            new_graph.ax.bar(new_df['date'], new_df[chart], alpha=0.5, label=new_country.title())
+        else:
+            new_graph.ax.plot(new_df['date'], new_df[chart], marker='.', label=new_country.title(), linewidth=2, markersize=12)
+    new_graph.ajust_graph(youngest, stardate)
+    plt.show()
