d10defa90e0a8b137058e74f65775f40851d5649
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-05-14 18:10:09 +0300

    fixed country list

--- main_script.py
+++ main_script.py
@@ -17,14 +17,6 @@ def import_data():
     df = results.dataframe
     return df
 
-def process_data(df):
-    '''
-    Sorts country names
-    '''
-    df.drop(df.loc[df['location'] == "Cote d'Ivoire"].index, inplace=True)
-    df['location'] = df['location'].str.lower()
-    return df
-
 def main():
     df = import_data()
     youngest = max(df['date'])
@@ -32,10 +24,11 @@ def main():
     print(f"Updated: {youngest}")
 
     plt.style.use('ggplot')
-    show_most_cases('3', df)
-    show_most_cases('4', df)
-    df = process_data(df)
+    #show_most_cases('3', df)
+    #show_most_cases('4', df)
+    df.drop(df.loc[df['location'] == "Cote d'Ivoire"].index, inplace=True)
     countries = np.sort(df['location'].unique())
+    df['location'] = df['location'].str.lower()
     compare_countries(df, countries, youngest)
 
 if __name__ == "__main__":
