32147e6468dea42939d043db9340b9eda8ed6338
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-05-14 11:45:25 +0300

    setting values to bars

--- topgraphs.py
+++ topgraphs.py
@@ -5,14 +5,16 @@ from classes import Graph
 
 def show_most_cases(chart, df):
     '''
-    Shows 20 countries statistic of most cases/deats
+    Shows 20 countries statistic of most cases/deaths
 
         param: chart name, dataframe
     '''
     stats= chart.split(sep='_')[-1]
-    graph2 = Graph('COVID-19 ' + stats + ' rate per country', 'countries', stats)
+    graph = Graph('COVID-19 ' + stats + ' rate per country', 'countries', stats)
     top20 = df.groupby('location')[[chart]].sum().sort_values([chart])[-20:-1].reset_index()
-    top20.plot(kind='barh', color='#336600', width=0.85, y=chart, x='location', ax=graph2.ax)
-    graph2.set_info()
-    graph2.ax.legend().set_visible(False)
+    top20.plot(kind='barh', color='#336600', width=0.85, y=chart, x='location', ax=graph.ax)
+    graph.set_info()
+    for i, country in enumerate(top20[chart]):
+        graph.ax.text(country, i, " "+str(country), va='center')
+    graph.ax.legend().set_visible(False)
     plt.show()
