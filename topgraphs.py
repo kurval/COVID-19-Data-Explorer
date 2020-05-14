18d507039d52d76f67f686d550096e43eca92a0b
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-05-14 13:15:56 +0300

    19  -> 20 countries

--- topgraphs.py
+++ topgraphs.py
@@ -21,7 +21,7 @@ def show_most_cases(chart, df):
     '''
     stats= chart.split(sep='_')[-1]
     graph = Graph('COVID-19 ' + stats[:-1] + ' rate per country', 'countries', stats)
-    top20 = df.groupby('location')[[chart]].sum().sort_values([chart])[-20:-1].reset_index()
+    top20 = df.groupby('location')[[chart]].sum().sort_values([chart])[-21:-1].reset_index()
     top20.plot(kind='barh', color=get_N_HexCol(), width=0.85, y=chart, x='location', ax=graph.ax)
     graph.set_info()
     for i, country in enumerate(top20[chart]):
