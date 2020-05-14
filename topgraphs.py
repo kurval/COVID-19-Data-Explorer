3b442b32027cb8662b3eba3a9ce27df00500fad6
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-05-14 10:58:17 +0300

    typo

--- topgraphs.py
+++ topgraphs.py
@@ -6,9 +6,11 @@ from classes import Graph
 def show_most_cases(chart, df):
     '''
     Shows 20 countries statistic of most cases/deats
+
+        param: chart name, dataframe
     '''
     stats= chart.split(sep='_')[-1]
-    graph2 = Graph('COVID-19 ' + stats + 'rate per country', 'countries', stats)
+    graph2 = Graph('COVID-19 ' + stats + ' rate per country', 'countries', stats)
     graph2.set_info()
     top20 = df.groupby('location')[[chart]].sum().sort_values([chart])[-20:-1].reset_index()
     top20.plot(kind='barh', color='#336600', width=0.85, y=chart, x='location', ax=graph2.ax)
