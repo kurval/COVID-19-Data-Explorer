f1350d192d40f102588540a08838d0ec5ea206ba
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-10-22 20:36:20 +0300

    .

--- test_cases/test_compare_countries.py
+++ test_cases/test_compare_countries.py
@@ -13,11 +13,11 @@ import time
 class CompareCountries(unittest.TestCase):
 
     def setUp(self):
-        #chrome_options = webdriver.chrome.options.Options()
-        #chrome_options.add_argument("--headless")
-        #chrome_options.add_argument('--no-sandbox')
-        #chrome_options.add_argument('--disable-dev-shm-usage')
-        self.driver = webdriver.Chrome()
+        chrome_options = webdriver.chrome.options.Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get("http://localhost:8501/http://covid19dataexplorer.com/dev")
 
     def getElement(self, attr):
