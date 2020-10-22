b861a06fff2e3a6ce86ac534c6911507b4f570d5
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-10-22 21:33:10 +0300

    ..

--- test_cases/test_most_cases.py
+++ test_cases/test_most_cases.py
@@ -19,6 +19,7 @@ class WorstHitCountries(unittest.TestCase):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument('--start-maximized')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=chrome_options)
