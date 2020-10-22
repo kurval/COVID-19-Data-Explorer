cbc4174ba3545d6656d12af8783d2e6ff0b1d3bc
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-10-22 21:05:50 +0300

    resizing window

--- test_cases/test_compare_countries.py
+++ test_cases/test_compare_countries.py
@@ -16,6 +16,7 @@ class CompareCountries(unittest.TestCase):
         chrome_options = webdriver.chrome.options.Options()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get("http://localhost:8501/http://covid19dataexplorer.com/dev")
