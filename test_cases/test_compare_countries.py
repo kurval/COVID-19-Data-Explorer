e9776246879da400f79f7b97452118d6c48f1883
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-10-23 11:12:44 +0300

    adding executable path and clean url

--- test_cases/test_compare_countries.py
+++ test_cases/test_compare_countries.py
@@ -17,8 +17,8 @@ class CompareCountries(unittest.TestCase):
         options.add_argument('-headless')
         options.add_argument("-width=1920")
         options.add_argument("-height=1080")
-        self.driver = webdriver.Firefox(options=options)
-        self.driver.get("http://localhost:8501/http://covid19dataexplorer.com/dev")
+        self.driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver', options=options)
+        self.driver.get("http://localhost:8501/covid19dataexplorer.com/dev")
 
     def getElement(self, attr):
         driver = self.driver
